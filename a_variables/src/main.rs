749043cf119f8ac47c0adb5271caeb95209788af
Esmaeil MIRZAEE <esmaeilmirzaee@gmail.com>
2020-12-30 16:52:09 -0500

    init

---
+++ a_variables/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    const NUMBER_OF_MISSILES = 9;
+    const NUMBER_OF_READY = 2;
+    let (missiles, ready) = (NUMBER_OF_MISSILES, NUMBER_OF_READY);
+    println!("Firing {} out of {}", ready, missiles);
+}
